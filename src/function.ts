c757166928c5b482ff2654d29f6a501ef444370e
Weiwei <linweiwei2222@163.com>
2024-01-25 19:51:51 -0500

    practice

---
+++ src/function.ts
@@ -0,0 +1,33 @@
+//-----------
+// functions
+//-----------
+
+function addTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const subtractTwoNumbers = (a: number, b: number): number => {
+  return a - b;
+};
+
+// addTwoNumbers('2', 5)
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 7);
+
+function addAllNumbers(items: number[]): void {
+  const total = items.reduce((a, c) => a + c, 0);
+  console.log(total);
+}
+
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
+
+//-----------------------
+// return type inference
+//-----------------------
+
+function formatGreeting(name: string, greeting: string) {
+  return `${greeting}, ${name}`;
+}
+
+// we get inference on return types, but not on argument types
+// type inference on return values does not enforce a return type
