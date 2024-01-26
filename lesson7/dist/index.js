153282b7eaa201457d237d98e63a61e42cb7b33e
Weiwei <linweiwei2222@163.com>
2024-01-26 18:22:03 -0500

    practice

---
+++ lesson7/dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+//----------
+// any type
+//----------
+let age;
+let title;
+age = 30;
+age = false;
+title = 25;
+title = {
+    hello: "world",
+};
+//--------------------
+// any type in arrays
+//--------------------
+let things = ["hello", true, 30, null];
+things.push({ id: 123 });
+//-----------------
+// functions & any
+//-----------------
+function addTogether(value) {
+    return value + value;
+}
+const resultOne = addTogether("hello");
+const resultTwo = addTogether(3);
+// useful when migrating from js to ts
+// because using any won't cause errors initially
---
+++ lesson7/src/index.ts
@@ -0,0 +1,36 @@
+//----------
+// any type
+//----------
+
+let age: any;
+let title;
+
+age = 30;
+age = false;
+
+title = 25;
+title = {
+  hello: "world",
+};
+
+//--------------------
+// any type in arrays
+//--------------------
+
+let things: any[] = ["hello", true, 30, null];
+
+things.push({ id: 123 });
+
+//-----------------
+// functions & any
+//-----------------
+
+function addTogether(value: any): any {
+  return value + value;
+}
+
+const resultOne = addTogether("hello");
+const resultTwo = addTogether(3);
+
+// useful when migrating from js to ts
+// because using any won't cause errors initially
