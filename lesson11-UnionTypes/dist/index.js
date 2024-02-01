84073e1734e5fd196cdb417a2f70b0d7d37c4047
Weiwei <linweiwei2222@163.com>
2024-02-01 18:00:22 -0500

    practice

---
+++ lesson11-UnionTypes/dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+//-------------
+// union types
+//-------------
+let someId;
+someId = 1;
+someId = "2";
+let email = null;
+email = "mario@netninja.dev";
+email = null;
+let anotherId;
+// anotherId = undefined
+anotherId = "1";
+anotherId = 2;
+//--------------------
+// union type pitfall
+//--------------------
+function swapIdType(id) {
+    // can only use props and methods common to
+    // both number and string types
+    // parseInt(id) --> not allowed
+    return id;
+}
